7d8a65c8c7b95ec4b49f93bfc4a7152a5fd97c55
Gyujin Cho <gyujincho@toss.im>
2025-02-24 01:04:13 +0900

    fix: minor style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import Navigation from './Navigation';
 
-export default function 메({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navigation />
-      <Box maxW="container.xl" mx="auto" px={4}>
+      <Box maxW="container.xl" mx="auto" pb={16} px={6}>
         {children}
       </Box>
     </>
